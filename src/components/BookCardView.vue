ec3b62d66b71220826786968f922ad9a71217072
KUMAR Akash O <AKR136@dsone.3ds.com>
2025-02-27 00:40:51 +0530

    added view action buttons, confirmation before delete and styling adjustment

--- src/components/BookCardView.vue
+++ src/components/BookCardView.vue
@@ -1,15 +1,31 @@
 <template>
-    <el-card v-for="book in booksdata" :key="book.id" style="max-width: 480px">
-      <img
-        src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-        style="width: 20%"
-      />
-      <div>
-        <p>{{ book.title }}</p>
-        <button @click="$emit('editBook', book)"><IconEdit style="width: 1em; height: 1em;"/></button>
-        <button @click="$emit('removeBook', book)"><IconDelete style="width: 1em; height: 1em;"/></button>
-      </div>
-    </el-card>
+  <div class="cardView">
+    <el-space wrap>
+      <el-card v-for="book in booksdata" :key="book.id" style="max-width: 220px">
+        <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+          style="width: 100%" />
+        <div>
+          <p>{{ book.title }}</p>
+          <p>Genre : {{ book.type }}</p>
+          <p>Author: {{ book.author }}</p>
+          <p>Price: {{ book.price }}</p>
+          <el-button @click="$emit('editBook', book)" circle>
+            <IconEdit style="width: 1em; height: 1em;" />
+          </el-button>
+          <el-popconfirm 
+            title="Are you sure to remove this?"
+            @confirm="handleDelete(book)"
+          >
+            <template #reference>
+              <el-button type="danger" circle>
+                <IconDelete style="width: 1em; height: 1em;" />
+              </el-button>
+            </template>
+          </el-popconfirm>
+        </div>
+      </el-card>
+    </el-space>
+  </div>
 </template>
 
 <script setup>
@@ -18,5 +34,15 @@ import IconDelete from "./icons/IconDelete.vue";
 
 
 const props = defineProps(['booksdata'])
-defineEmits(['removeBook', 'editBook'])
+const emit = defineEmits(['removeBook', 'editBook'])
+
+const handleDelete = (book)=>{
+    emit('removeBook', book.id)
+}
 </script>
+
+<style>
+.cardView {
+  margin-bottom: 20px;
+}
+</style>
--- src/components/Books.vue
+++ src/components/Books.vue
@@ -1,33 +1,39 @@
-<template>  
-  <input v-model="newBook.title" placeholder="Title"/>
-  <input v-model="newBook.author" placeholder="Author"/>
-  <input v-model="newBook.price" placeholder="Price"/>
-  <button :style='{marginLeft:"5px",}' @click="addItem">{{ newBook.btnStatus }}</button>
+<template>
+  <input v-model="newBook.title" placeholder="Title" />
+  <input v-model="newBook.author" placeholder="Author" />
+  <input v-model="newBook.price" placeholder="Price" />
+  <button :style='{ marginLeft: "5px", }' @click="addItem">{{ newBook.btnStatus }}</button>
   <div>
-    <input type="radio" id="horror" value="horror" v-model="newBook.type"/>
+    <input type="radio" id="horror" value="Horror" v-model="newBook.type" />
     <label for="horror">Horror</label>
 
-    <input type="radio" id="scienceFiction" value="scienceFiction" v-model="newBook.type" />
+    <input type="radio" id="scienceFiction" value="Science Fiction" v-model="newBook.type" />
     <label for="scienceFiction">Science Fiction</label>
 
-    <input type="radio" id="romance" value="romance" v-model="newBook.type" />
+    <input type="radio" id="romance" value="Romance" v-model="newBook.type" />
     <label for="romance">Romance</label>
 
-    <input type="radio" id="thriller" value="thriller" v-model="newBook.type"/>
+    <input type="radio" id="thriller" value="Thriller" v-model="newBook.type" />
     <label for="thriller">Thriller</label>
 
-    <input type="radio" id="fantasy" value="fantasy" v-model="newBook.type" />
+    <input type="radio" id="fantasy" value="Fantasy" v-model="newBook.type" />
     <label for="fantasy">Fantasy</label>
   </div>
+  <div>
+    <el-button type="success" :icon="Plus">Add New Book</el-button>
+    <el-button-group class="ml-4">
+      <el-button type="primary" :icon="Grid" @click="currentView = 'grid'" />
+      <el-button type="primary" :icon="Files" @click="currentView = 'card'" />
+    </el-button-group>
+  </div>
 
-  <Book :booksdata="booksdata" @removeBook="removeBook" @editBook="editBook"/>
-  <!-- <BookCardView :booksdata="booksdata" @removeBook="removeBook" @editBook="editBook" /> -->
-  <BookGridView :booksdata="booksdata" @removeBook="removeBook" @editBook="editBook"/>
+  <BookCardView v-if="currentView === 'card'" :booksdata="booksdata" @removeBook="removeBook" @editBook="editBook" />
+  <BookGridView v-if="currentView === 'grid'" :booksdata="booksdata" @removeBook="removeBook" @editBook="editBook" />
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import Book from './Book.vue'
+import { Plus, Grid, Files} from '@element-plus/icons-vue'
 import BookCardView from './BookCardView.vue'
 import BookGridView from './BookGridView.vue'
 
@@ -35,35 +41,37 @@ import BookGridView from './BookGridView.vue'
 const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
 
 const booksdata = ref([
-  {id: 1, title: "The Gravity", type: "scienceFiction", author:"123",price: 10},
-  {id: 2, title: "Intesteller", type: "scienceFiction", author:"456",price: 20},
-  {id: 3, title: "Doremon", type: "romance", author:"789",price: 25},
-  {id: 4, title: "Sinchan", type: "romance", author:"101",price: 15},
-  {id: 5, title: "Annabell", type: "horror", author:"102",price: 5},
-  {id: 6, title: "Ouija", type: "horror", author:"103",price: 100},
-  {id: 7, title: "Never Lie", type: "thriller", author:"104",price: 12},
-  {id: 8, title: "The Locked Door", type: "thriller", author:"105",price: 23},
-  {id: 9, title: "I too had a love story", type: "fantasy", author:"106",price: 21},
-  {id: 10, title: "Can Love happens twice", type: "fantasy", author:"107",price: 11}
+  { id: 1, title: "The Gravity", type: "Science Fiction", author: "123", price: 10 },
+  { id: 2, title: "Intesteller", type: "Science Fiction", author: "456", price: 20 },
+  { id: 3, title: "Doremon", type: "Romance", author: "789", price: 25 },
+  { id: 4, title: "Sinchan", type: "Romance", author: "101", price: 15 },
+  { id: 5, title: "Annabell", type: "Horror", author: "102", price: 5 },
+  { id: 6, title: "Ouija", type: "Horror", author: "103", price: 100 },
+  { id: 7, title: "Never Lie", type: "Thriller", author: "104", price: 12 },
+  { id: 8, title: "The Locked Door", type: "Thriller", author: "105", price: 23 },
+  { id: 9, title: "I too had a love story", type: "Fantasy", author: "106", price: 21 },
+  { id: 10, title: "Can Love happens twice", type: "Fantasy", author: "107", price: 11 }
 ])
 
 const newBook = ref({
-  id: "", 
-  title: "", 
-  type: "", 
-  author:"",
+  id: "",
+  title: "",
+  type: "",
+  author: "",
   price: 0,
   btnStatus: "Add"
 })
 
-let addItem = (book)=> {
-  if(!newBook.value.title || !newBook.value.type){
+const currentView = ref("grid");
+
+let addItem = (book) => {
+  if (!newBook.value.title || !newBook.value.type) {
     alert("Please, Provide both values!")
-    return ;
+    return;
   }
 
   const tempData = {
-    id: newBook.value.btnStatus === "Add"? Date.now() + Math.random(): newBook.value.id,
+    id: newBook.value.btnStatus === "Add" ? Date.now() + Math.random() : newBook.value.id,
     title: newBook.value.title,
     type: newBook.value.type,
     author: newBook.value.author,
@@ -71,11 +79,11 @@ let addItem = (book)=> {
   }
   console.log(tempData);
 
-  if(newBook.value.btnStatus === "Add"){
+  if (newBook.value.btnStatus === "Add") {
     booksdata.value.push(tempData);
-  }else{
+  } else {
     booksdata.value.forEach(book => {
-      if(book.id === tempData.id){
+      if (book.id === tempData.id) {
         book.title = tempData.title;
         book.type = tempData.type;
         book.author = tempData.author;
@@ -86,7 +94,7 @@ let addItem = (book)=> {
   resetValues();
 }
 
-let editBook = (book)=>{
+let editBook = (book) => {
   newBook.value.id = book.id;
   newBook.value.title = book.title;
   newBook.value.type = book.type;
@@ -95,17 +103,17 @@ let editBook = (book)=>{
   newBook.value.btnStatus = 'Update';
 }
 
-let removeBook = (id)=>{
+let removeBook = (id) => {
   console.log(id);
   booksdata.value = booksdata.value.filter(book => book.id !== id);
 }
 
-const resetValues = ()=>{
+const resetValues = () => {
   newBook.value = {
     id: "",
     book: "",
     bookType: "",
-    author:"",
+    author: "",
     price: 0,
     btnStatus: "Add"
   }
