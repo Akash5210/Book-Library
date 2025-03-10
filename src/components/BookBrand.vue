e330be0f74e5836d105a2e8c791ae2ca34c3d924
KUMAR Akash O <AKR136@dsone.3ds.com>
2025-03-10 16:09:14 +0530

    README file updated and confirmation before loggout is added

--- src/components/BookBrand.vue
+++ src/components/BookBrand.vue
@@ -6,11 +6,25 @@
         <el-button type="primary" @click="loggedInAccess.setShowLoginModal(true)">Login</el-button>
         <el-button plain @click="loggedInAccess.setShowRegisterModal(true)">Register</el-button>
       </span>
-      <el-button v-else @click="handleLogout" type="danger">Logout</el-button>
+      <!-- <el-button v-else @click="handleLogout" type="danger">Logout</el-button> -->
+      <el-button v-else @click="centerDialogVisible = true" type="danger">Logout</el-button>
+
     </div>
   </div>
+  <el-dialog v-model="centerDialogVisible" title="Are you sure to logout?" width="500" align-center>
+    <!-- <span>Open the dialog from the center from the screen</span> -->
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="centerDialogVisible = false">Cancel</el-button>
+        <el-button type="danger" @click="handleLogout">
+          Logout
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
 </template>
 <script setup>
+import { ref } from 'vue';
 import { logoutUser } from '@/auth';
 
 import { useLoggedInAccessStore } from '@/stores/loggedInAccess'
@@ -24,12 +38,20 @@ defineProps({
   },
 })
 
+const centerDialogVisible = ref(false)
+
 const handleLogout = async () => {
   await logoutUser();
   // Redirect or update UI after logout
   loggedInAccess.setIsLoggedIn(false);
+  centerDialogVisible.value = false;
   console.log("You are Logged Out successfully")
 };
 </script>
 <style scoped>
+.greetings {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 </style>
